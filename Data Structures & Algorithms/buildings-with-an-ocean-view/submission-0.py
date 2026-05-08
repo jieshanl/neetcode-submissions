38c658303d82fd4b5bebf41510b160306270b7b2
Shannon Liu <jieshanl23@berkeley.edu>
2026-05-07 22:28:42 -0700

    Add: buildings-with-an-ocean-view - submission-0

---
+++ Data Structures & Algorithms/buildings-with-an-ocean-view/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
+        # Use the greedy algorithm
+        # start iterating from the last index
+
+        res = [len(heights) - 1]
+
+        for i in range(len(heights) - 2, -1, -1):
+            if heights[i] > heights[res[-1]]:
+                res.append(i)
+        res.reverse()
+        return res
