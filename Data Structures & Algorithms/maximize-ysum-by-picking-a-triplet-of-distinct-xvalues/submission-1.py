84c73c4e824cab85f91003ac925a7f8fd3f71d25
Shannon Liu <jieshanl23@berkeley.edu>
2026-05-08 13:50:50 -0700

    Add: maximize-ysum-by-picking-a-triplet-of-distinct-xvalues - submission-1

---
+++ Data Structures & Algorithms/maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxSumDistinctTriplet(self, x: List[int], y: List[int]) -> int:
+        # Use the Hash Map + Min-Heap data structures
+        # HashMap: map the x[i] to y[i]
+
+        mp = {}
+        for xi, yi in zip(x, y): # [1, 2, 1, 3, 2, 5, 3, 4, 6, 2]
+            mp[xi] = max(mp.get(xi, 0), yi)
+        
+        # now building out minHeap to store the mp.values()
+        minHeap = []
+        for val in mp.values():
+            heapq.heappush(minHeap, val)
+            # check if length of minHeap is greater than 3
+            if len(minHeap) > 3:
+                heapq.heappop(minHeap)
+        
+        return -1 if len(minHeap) < 3 else sum(minHeap)
+
+        # Time Complexity: O(n)
+        # Space Complexity: O(1)
