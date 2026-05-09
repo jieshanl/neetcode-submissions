5d8516bfa037fe920fd620c0d82c0024d33cad62
Shannon Liu <jieshanl23@berkeley.edu>
2026-05-09 11:59:57 -0700

    Add: count-servers-that-communicate - submission-1

---
+++ Data Structures & Algorithms/count-servers-that-communicate/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        # Iteration
+
+        ROWS, COLS = len(grid), len(grid[0])
+
+        row_cnt = [0] * ROWS # row -> cnt
+        col_cnt = [0] * COLS # col -> cnt
+
+        # Preprocessing
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    row_cnt[r] += 1
+                    col_cnt[c] += 1
+        
+        res = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] and (row_cnt[r] > 1 or col_cnt[c] > 1):
+                    res += 1
+        return res
+
+        # Time Complexity: O(m * n)
+        # Space Complexity: O(m + n)
+
