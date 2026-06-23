f1bd580510b9a091104d1397dc34198a345df051
Shannon Liu <jieshanl23@berkeley.edu>
2026-06-23 16:26:38 -0700

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set() # initialize an empty set "seen"
+
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+
+        # Time Complexity: O(n)
+        # Space Complexity: O(n)
